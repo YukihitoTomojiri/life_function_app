564a5f96b6ea94ee39709e4e895961b847fd791b
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-02-25 21:03:06 +0900

    利用者登録機能の実装

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,12 +1,33 @@
 class PatientsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  # before_action :move_to_index, except: [:index, :show]
   def index
   end
 
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  
+
   private
+  
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+
+  def patient_params
+    params.require(:patient).permit(:patient_last_name, :patient_first_name, :patient_last_name_kana, :patient_first_name_kana, :gender_id, 
+                                    :birthday, :care_certified_id, :prefecture_id).merge(user_id: current_user.id)
   end
 end

a85e9b55877b20e18b5d9ef70313512eaa28f86b
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2022-03-01 22:00:04 +0900

    routingの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'hdsrs/index'
+  get 'hdsrs/create'
   devise_for :users
   root to: 'patients#index'
-  resources :patients, only: [:index, :new, :create, :show] 
+  resources :patients, only: [:index, :new, :create, :show] do
+    resources :hdsrs, only: [:index, :create] 
+  end
+
 
 end

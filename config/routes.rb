5aec96b4420fbc14a8cf048eb64285127a9a9f11
YukihitoTomojiri <i.can.do.it.0922@gmail.com>
2022-03-18 20:37:30 +0900

    routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'patients#index'
-  resources :patients, only: [:index, :new, :create, :show] do
-    resources :hdsr_logs, only: [:index, :create, :show] 
+  resources :patients, only: [:index, :new, :create, :show, :destroy] do
+    resources :hdsr_logs, only: [:index, :create, :show, :destroy] 
   end
 
 

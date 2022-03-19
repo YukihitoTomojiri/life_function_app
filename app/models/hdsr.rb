04cb2b603ffd5873c4b287e16cec9aa601dc0f6e
YukihitoTomojiri <i.can.do.it.0922@gmail.com>
2022-03-19 17:41:47 +0900

    Update hdsr.rb

--- app/models/hdsr.rb
+++ app/models/hdsr.rb
@@ -1,8 +1,5 @@
 class Hdsr < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :answer_0to1
-  belongs_to_active_hash :answer_0to2
-  belongs_to_active_hash :answer_0to5
 
   has_many :users
   has_many :patients

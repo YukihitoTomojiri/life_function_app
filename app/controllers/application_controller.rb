ce4fba3ddcb6b674834e8135a81809c5e23b1ac4
YukihitoTomojiri <i.can.do.it.0922@gmail.com>
2022-04-16 21:46:41 +0900

    ログイン後の遷移先を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :basic_auth
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   private
 
   def basic_auth
